40dc17c4ec3488ebe607aabe5fa96ca5296af37e
Jacques Alberts <server.jac@gmail.com>
2016-10-31 14:20:41 +0200

    Created SidebarComponent and TopnavComponent.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedComponent } from './shared.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { TopnavComponent } from './topnav/topnav.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [SharedComponent]
+  declarations: [SharedComponent, SidebarComponent, TopnavComponent]
 })
 export class SharedModule {
 
