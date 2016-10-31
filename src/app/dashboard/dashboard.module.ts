9e4a0f0452af9f7a69ab3462e387ddbf0090908c
Jacques Alberts <server.jac@gmail.com>
2016-10-31 13:26:47 +0200

    Setup DashboardModule routing.

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 
+import { DashboardRoutingModule } from './dashboard-routing.module.ts';
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    DashboardRoutingModule
   ],
   declarations: [DashboardComponent]
 })
-export class DashboardModule { }
+export class DashboardModule {
+
+  constructor() {
+    console.log('DashboardModule constructor');
+  }
+
+}
