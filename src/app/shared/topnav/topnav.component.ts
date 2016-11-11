f67ff32bddaf368366c94a97d8641a7b73d2cb79
Jacques Alberts <server.jac@gmail.com>
2016-11-11 09:03:18 +0200

    Topnav update with logged in user.

--- src/app/shared/topnav/topnav.component.ts
+++ src/app/shared/topnav/topnav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/index';
 
 @Component({
   selector: 'app-topnav',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopnavComponent implements OnInit {
 
-  constructor() { }
+  private loggedInUser$;
+
+  // Other option is to do something similar to this:
+  // private _isLoggedIn: Boolean = false;
+  // ngOnInit() {
+  //   this.loggedInUser$ = this.authService.loggedInUser$;
+  //
+  //   // Remember to unsubscribe in ngOnDestroy.
+  //   this.loggedInUserSubscription = this.loggedInUser$.subscribe(user => {
+  //     if(user) {
+  //       this._isLoggedIn = true;
+  //     } else {
+  //       this._isLoggedIn = false;
+  //     }
+  //   });
+  // }
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.loggedInUser$ = this.authService.loggedInUser$;
   }
+
+  // ngOnDestroy() {
+    // Async pipes automatically unsubscribe from observables.
+    // If you manually subscribe to them you will have to manually
+    // unsubscribe them in ngOnDestroy to prevent a memory leak when
+    // the component is destroyed.
+    // e.g.
+    // If you do this:
+    // this.loggedInUserSubscription = this.loggedInUser$.subscribe(...);
+    // then you have to do this in ngOnDestroy:
+    // this.loggedInUserSubscription.unsubscribe();
+  // }
 
 }
